6f9979d06e48e7d454fc04268ad0513d825e208b
Kateryna Berehovych <kasia-94@ukr.net>
2022-08-14 16:09:12 +0300

    Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
+import '../css/common.css';
+import '../css/03-feedback.css';
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+populateTextarea();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { email, message },
+  } = e.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  console.log({ email: email.value, message: message.value });
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  const formDataJSON = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, formDataJSON);
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  if (savedMessage) {
+    console.log(savedMessage);
+    const savedMessageParsed = JSON.parse(savedMessage);
+    Object.keys(savedMessageParsed).forEach(
+      key => (form[key].value = savedMessageParsed[key])
+    );
+  }
+}
