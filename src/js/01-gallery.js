8ff40f92d48e29677710ca0061dec3f4a3f6171a
Kateryna Berehovych <kasia-94@ukr.net>
2022-08-14 11:34:03 +0300

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import '../css/common.css';
+import '../css/01-gallery.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+const paletteContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCards(galleryItems);
+
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGalleryCards(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+      <li><a class="gallery__item" href="${original}">
+      <img class = 'gallery__image' src="${preview}"
+      data-source="${original}" 
+      alt="${description}"/>
+      </a></li>`;
+    })
+    .join('');
+}
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  showCounter: false,
+  overlayOpacity: 0.9,
+});
